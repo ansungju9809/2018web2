b65332b78f83b6485936b881e43330bbd2b4f723
sesdf <qwert2687@daum.net>
2018-04-23 10:40:55 +0900

    학교홈페이지 4월23일자

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -1,3 +1,22 @@
-$(document).ready(function () {
-    
+var img=$("#slide img");
+var max=img.length-1;
+var cnt=0;
+
+function slide() {
+    $(img[cnt]).animate({"left":"100%"},function () {
+        $(this).css({"left":"-100%"});
+    });
+    cnt++;
+    if(cnt>max) cnt=0;
+    $(img[cnt]).animate({"left":"0"});
+}
+setInterval(slide,2000);
+
+$(".notice").click(function () {
+    $("#notice").css({"display":"block"});
+    $("#h_notice").css({"display":"none"});
+})
+$(".h_notice").click(function () {
+    $("#notice").css({"display":"none"});
+    $("#h_notice").css({"display":"block"});
 })

1352be2e57c617296089418dfdb2354aa3718239
sesdf <qwert2687@daum.net>
2018-04-23 14:06:49 +0900

    학교홈페이지 4월23일자

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -13,10 +13,24 @@ function slide() {
 setInterval(slide,2000);
 
 $(".notice").click(function () {
-    $("#notice").css({"display":"block"});
-    $("#h_notice").css({"display":"none"});
-})
+    $("#notice").css({"height":"300px"});
+    $("#h_notice").css({"height":"0px"});
+});
 $(".h_notice").click(function () {
-    $("#notice").css({"display":"none"});
-    $("#h_notice").css({"display":"block"});
-})
+    $("#notice").css({"height":"0px"});
+    $("#h_notice").css({"height":"300px"});
+});
+
+var pop=$("#popup img");
+var pop_max=img.length-1;
+var i=0;
+
+function pop_slide() {
+    $(pop[i]).animate({"left":"100%"},function () {
+        $(this).css({"left":"-100%"});
+    });
+    i++;
+    if(i>pop_max) i=0;
+    $(pop[i]).animate({"left":"0"});
+}
+setInterval(pop_slide,3000);
